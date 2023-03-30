3b85217e981436f9ac685eba8e8a9dfab920ef71
Oleh Terno <dzonoleg@gmail.com>
2023-03-30 10:21:00 +0300

    add button

--- pogoda_here/Program.cs
+++ pogoda_here/Program.cs
@@ -4,7 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace pogoda_here
 {
@@ -14,7 +14,7 @@ namespace pogoda_here
         {
             string FilePath = @"C:\Users\dzono\source\repos\pogoda_here\token.txt";
             string token = System.IO.File.ReadAllText(FilePath);
-            var client = new TelegramBotClient(token);
+            var client = new TelegramBotClient(token) ;
             client.StartReceiving(Update, Error);
             
             Console.ReadLine();
@@ -24,7 +24,7 @@ namespace pogoda_here
         {
             throw new NotImplementedException();
         }
-        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken CansToken)
         {
             var message = update.Message;
             if (message.Text != null)
@@ -33,22 +33,37 @@ namespace pogoda_here
                 if (message.Text.ToLower().Contains("/start"))
                 {
                     await botClient.SendTextMessageAsync(message.Chat.Id, "Hi! I'm a bot and i'll help you with weather");
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Please, write /keyboard");
                 }
-                if (message.Text.ToLower().Contains("/loc"))
+                else if(message.Text.ToLower().Contains("/keyboard"))
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, "Please write your location!");
-                }
-                if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
-                {
-                    if (update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                    ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup(new KeyboardButton[][]
                     {
+                        new KeyboardButton[] { new KeyboardButton("Choose location"), new KeyboardButton("Weather forecast") },
+                        new KeyboardButton[] { new KeyboardButton("for feature"), new KeyboardButton("for feature21") }
+                    });
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Choose a option",replyMarkup: keyboard);
+                }
+                
+                        await botClient.SendTextMessageAsync(message.Chat.Id, "Enter your location");
                         var text = update.Message.Text;
                         var id = update.Message.Chat.Id;
                         string? username = update.Message.Chat.Username;
                         Console.WriteLine(text);
-                    }
-                }
+                        
+                
+
+                //if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message && update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                //{
+                //    var text = update.Message.Text;
+                //    var id = update.Message.Chat.Id;
+                //    string? username = update.Message.Chat.Username;
+                //    Console.WriteLine(text);
+                //}
             }
         }
+
     }
+
 }
+
